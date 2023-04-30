9b6b93ec7db59f1a6e83f66193fe3018a3dfd808
sathishkannan162 <sathishkannan162@gmail.com>
2023-04-30 19:08:22 +0530

    fix: use aws serverless lambda

--- src/http/http-common.js
+++ src/http/http-common.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // const baseURL = 'http://localhost:8080/api';
-const baseURL = "https://chatbot-voice-backend.sathishkannan16.repl.co/api";
+const baseURL = "https://q8ete8xxul.execute-api.us-east-1.amazonaws.com/"
 
 const httpCommon = axios.create({
   baseURL: baseURL,
