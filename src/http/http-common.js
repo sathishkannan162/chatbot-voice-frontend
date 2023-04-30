defc53e87bd254b50d7a5c670003f1bf2207a823
sathishkannan162 <sathishkannan162@gmail.com>
2023-04-30 10:11:10 +0530

    feat: changed baseUrl for axios

--- src/http/http-common.js
+++ src/http/http-common.js
@@ -1,7 +1,8 @@
 // import axios and set base url
 import axios from "axios";
 
-const baseURL = "http://localhost:8080/api";
+// const baseURL = "http://localhost:8080/api";
+const baseURL = "https://chatbot-voice-backend.sathishkannan16.repl.co/api";
 
 const httpCommon = axios.create({
   baseURL: baseURL,
