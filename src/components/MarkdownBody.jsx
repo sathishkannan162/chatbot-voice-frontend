c0e27ee66cd01a4dc532fa6b2dcfaa001868ccbe
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-07 18:59:45 +0530

    feat: add copy code button to unhighlighted code blocks

--- src/components/MarkdownBody.jsx
+++ src/components/MarkdownBody.jsx
@@ -30,20 +30,20 @@ export default function MarkdownBody(props) {
       components={{
         code({ node, inline, className, children, ...props }) {
           const match = /language-(\w+)/.exec(className || '');
-          return !inline && match ? (
+          return !inline ? (
             <div className="markdown-code-body">
               <Box
                 className="code-header"
                 sx={{
-                  display: 'flex',
+                  display: match ? 'flex' : 'block',
                   justifyContent: 'space-between',
-                  textAlign: 'left',
+                  textAlign: match ? 'left' : 'right',
                   color: light ? copyIconColorLight : copyIconColorDark, // TODO:uncomment when implementing dark mode
                   // color: 'purple',
                   // NOTE: set color to grey or some other suitable color for dark mode and do the same for light mode.
                 }}
               >
-                {match[1]}
+                {match ? match[1] : ' '}
                 {copy ? (
                   <Tooltip title="copied" placement="right-end">
                     <AssignmentTurnedInIcon fontSize="small" />
@@ -58,13 +58,17 @@ export default function MarkdownBody(props) {
                 )}
               </Box>
               <div hidden>{(codeBlockTemp = children)}</div>
-              <SyntaxHighlighter
-                {...props}
-                children={String(children).replace(/\n$/, '')}
-                style={light ? materialLight : gruvboxDark}
-                language={match[1]}
-                PreTag="div"
-              />
+              {match ? (
+                <SyntaxHighlighter
+                  {...props}
+                  children={String(children).replace(/\n$/, '')}
+                  style={light ? materialLight : gruvboxDark}
+                  language={match[1]}
+                  PreTag="div"
+                />
+              ) : (
+                children
+              )}
             </div>
           ) : (
             <code {...props} className={className}>
