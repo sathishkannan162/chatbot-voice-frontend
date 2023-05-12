9198e1d2487b9233811556415197c651eeec74f8
sathishkannan162 <sathishkannan162@gmail.com>
2023-05-12 10:53:02 +0530

    feat: messages are stacked and appear on sides with respect to role

--- src/chakraComponents/Message.jsx
+++ src/chakraComponents/Message.jsx
@@ -1,18 +1,27 @@
 import { Card, CardBody, Avatar, Flex, Text } from '@chakra-ui/react';
 import robot from '../assets/robot-avatar.png';
-import user from '../assets/user-avatar.png' 
+import user from '../assets/user-avatar.png';
 export default function Message(props) {
   const { message } = props;
-  const isUser = message.role==='user';
+  const isUser = message.role === 'user';
 
   return (
-    <Card>
-      <CardBody>
-        <Flex gap="4" flexDir={isUser?"row":"row-reverse"} >
-          <Avatar name={isUser?'user':'assistant'} src={isUser?  user: robot}></Avatar> 
-          <Text>{message.content}</Text>
-        </Flex>
-      </CardBody>
-    </Card>
+    <Flex direction={isUser ? 'row-reverse' : 'row'}>
+      <Card maxW={{ base: '100%', sm: '100%', md: '85%' }} boxShadow="md">
+        <CardBody paddingY={2}>
+          <Flex
+            gap="2"
+            flexDir={isUser ? 'row-reverse' : 'row'}
+            alignItems="center"
+          >
+            <Avatar
+              name={isUser ? 'user' : 'assistant'}
+              src={isUser ? user : robot}
+            ></Avatar>
+            <Text>{message.content}</Text>
+          </Flex>
+        </CardBody>
+      </Card>
+    </Flex>
   );
 }
